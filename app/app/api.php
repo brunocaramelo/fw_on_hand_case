8008feb8465dbdd750ee86b8d2aa7f01688c5129
brunocaramelo <bruno.caramelo5@gmail.com>
2019-01-02 21:57:54 -0200

    ajuste migrate

--- app/app/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// $route[] = ['/api/v1/create-contact', '\App\Users\Controllers\UserController@login'];
-// $route[] = ['/api/v1/create-list', '\App\Users\Controllers\UserController@login'];
-// $route[] = ['/api/v1/create-message', '\App\Users\Controllers\UserController@login'];
-// $route[] = ['/api/v1/send-message', '\App\Users\Controllers\UserController@login'];
-$route[] = ['/api/v1/show-results', '\App\Integration\Results\Controllers\ResultsController@showResults','auth','show-message-result'];
-
-return $route;
